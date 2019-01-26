2685e2d785281105a0b3f0f060e8f01f189942c7
jayant <sikarwar.jayant@gmail.com>
2019-01-26 23:07:11 +0530

    test

--- Hellofresh.py
+++ Hellofresh.py
@@ -10,6 +10,7 @@ if __name__ == "__main__"(argv):
         .builder \
         .appName("Hellofresh_test") \
         .getOrCreate()
+    print('test')
 
     for arg in argv:
         if sys.argv[1]:
